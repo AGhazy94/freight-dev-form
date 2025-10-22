cba11eb3a707880faa6ad35459fe28873fb4f971
Ahmed Ghazy <ahmed.h.ghazy@gmail.com>
2025-10-23 02:54:44 +0300

    refactor: rename build script and update Vite base path; add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/freight-dev-form/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/REPONAME/' : './',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
